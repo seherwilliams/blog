58a1357e639cb05d24662ab3b73093bee28a7bca
seherwilliams <seherwilliams@gmail>
2023-09-07 14:11:47 +0800

    '数据库配置'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',# 数据库引擎
+        'HOST': '127.0.0.1',# 数据库主机
+        'PORT': 3306,# 数据库端口
+        'USER': 'kyq_blog',
+        'PASSWORD': '123',
+        'NAME': 'kyq_blog'
     }
 }
 
